c5a95932f4e5f94269dcd8b914e4086adb6c4924
Na-kyung Bae <162648287+backorground@users.noreply.github.com>
2024-08-28 01:56:04 +0900

    Update stop_watch_csec.v

--- HARMAN_PROJECT/multy_purpose_watch (Vivado)/stop_watch_csec.v
+++ HARMAN_PROJECT/multy_purpose_watch (Vivado)/stop_watch_csec.v
@@ -11,6 +11,12 @@ module stop_watch_csec(
     wire lap_swatch, lap_load;
     reg [15:0] lap_time;
     wire [15:0] cur_time;
+
+    //동작
+    // start_stop을 1로 만들면서(btn[1]을 누르면서) '초: 밀리초' 로 FND에 출력하면서 현재 시간이 흘러간다.
+    //btn[1]을 누르면 start_stop이 토글 되고 btn[2]를 누르면 lap_swatch 또한 토글 된다.
+    // lap_swatch가 1이 되는 순가 lap_load라는 엣지가 발생되면 그 순간 lap_time에 cur_time을 저장하고 lap_time을 FND에 출력한다.
+    // btn[2]을 눌러 lap_time을 0으로 만들면 안보이지만 흘러가던 cur_time이 출력된다.
     
     clock_usec usec_clk(clk_start, reset_p, clk_usec);
     clock_div_1000 msec_clk(clk_start, reset_p, clk_usec, clk_msec);
@@ -24,7 +30,7 @@ module stop_watch_csec(
     counter_dec_100 counter_csec(clk, reset_p, clk_csec, csec1, csec10);
     counter_dec_60 counter_sec(clk, reset_p, clk_sec, sec1, sec10);
     
-    T_flip_flop_p tff_lap(.clk(clk), .reset_p(reset_p), .t(btn_pedge[1]), .q(lap_swatch));
+    T_flip_flop_p tff_lap(.clk(clk), .reset_p(reset_p), .t(btn_pedge[2]), .q(lap_swatch));
     
     edge_detector_n ed(.clk(clk), .reset_p(reset_p), .cp(lap_swatch), .p_edge(lap_load));
     
